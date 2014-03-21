6251e0ef4f7a9de42c3d16fa60370cebf0c5ac83
Bradley Holloway <drummerboy400@gmail.com>
2014-03-20 19:12:08 -0500

    practice bots ports

--- src/org/TexasTorque/TexasTorque2014/constants/Ports.java
+++ src/org/TexasTorque/TexasTorque2014/constants/Ports.java
@@ -16,20 +16,20 @@ public class Ports {
     public final static int RIGHT_STRAFE_DRIVE_SIDECAR = 1;
     public final static int RIGHT_REAR_DRIVE_SIDECAR = 1;
 
-    public final static int RIGHT_FRONT_DRIVE_MOTOR_PORT = 10;
-    public final static int RIGHT_STRAFE_DRIVE_MOTOR_PORT = 3;
-    public final static int RIGHT_REAR_DRIVE_MOTOR_PORT = 1;
+    public final static int RIGHT_FRONT_DRIVE_MOTOR_PORT = 1;
+    public final static int RIGHT_STRAFE_DRIVE_MOTOR_PORT = 5;
+    public final static int RIGHT_REAR_DRIVE_MOTOR_PORT = 6;
 
     public final static int FRONT_INTAKE_TILT_SIDECAR = 1;
-    public final static int FRONT_INTAKE_TILT_MOTOR_PORT = 2;
+    public final static int FRONT_INTAKE_TILT_MOTOR_PORT = 3;
 
     public final static int FRONT_INTAKE_SIDECAR = 1;
     public final static int FRONT_INTAKE_MOTOR_PORT = 4;
 
     public final static int CATAPULT_SIDECAR = 2;
-    public final static int CATAPULT_MOTOR_PORT = 3;
+    public final static int CATAPULT_MOTOR_PORT = 2;
     public final static int CATAPULT_B_SIDECAR = 1;
-    public final static int CATAPULT_B_MOTOR_PORT = 9;
+    public final static int CATAPULT_B_MOTOR_PORT = 2;
 
     //----- Sidecar 2 -----
     public final static int LEFT_FRONT_DRIVE_SIDECAR = 2;
@@ -37,14 +37,14 @@ public class Ports {
     public final static int LEFT_REAR_DRIVE_SIDECAR = 2;
 
     public final static int LEFT_FRONT_DRIVE_MOTOR_PORT = 1;
-    public final static int LEFT_STRAFE_DRIVE_MOTOR_PORT = 7;
-    public final static int LEFT_REAR_DRIVE_MOTOR_PORT = 9;
+    public final static int LEFT_STRAFE_DRIVE_MOTOR_PORT = 5;
+    public final static int LEFT_REAR_DRIVE_MOTOR_PORT = 6;
 
     public final static int REAR_INTAKE_TILT_SIDECAR = 2;
-    public final static int REAR_INTAKE_TILT_MOTOR_PORT = 8;
+    public final static int REAR_INTAKE_TILT_MOTOR_PORT = 3;
 
     public final static int REAR_INTAKE_SIDECAR = 2;
-    public final static int REAR_INTAKE_MOTOR_PORT = 10;
+    public final static int REAR_INTAKE_MOTOR_PORT = 4;
 
     //----- Solenoids -----
     public final static int FRONT_DRIVEBASE_SWITCHER = 1;
@@ -89,8 +89,8 @@ public class Ports {
     //public final static int GYRO_PORT = 0;
     //public final static int ANALOG_PRESSURE_PORT = 0;
     
-    public final static int FRONT_INTAKE_TILT_POT_PORT = 7;
-    public final static int REAR_INTAKE_TILT_POT_PORT = 1;
+    public final static int FRONT_INTAKE_TILT_POT_PORT = 8;
+    public final static int REAR_INTAKE_TILT_POT_PORT = 7;
 
     //----- Relays -----
     //----- Sidecar 1 -----
