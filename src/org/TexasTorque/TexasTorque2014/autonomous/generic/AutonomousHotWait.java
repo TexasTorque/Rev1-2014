eac74c973133f050ba13286e74376b21e8a479f8
Bradley Holloway <drummerboy400@gmail.com>
2014-04-24 15:13:12 -0500

    Counts for one ball hot

--- src/org/TexasTorque/TexasTorque2014/autonomous/generic/AutonomousHotWait.java
+++ src/org/TexasTorque/TexasTorque2014/autonomous/generic/AutonomousHotWait.java
@@ -6,37 +6,36 @@ import org.TexasTorque.TexasTorque2014.autonomous.AutonomousCommand;
 import org.TexasTorque.TorqueLib.component.CheesyVisionServer;
 
 public class AutonomousHotWait extends AutonomousCommand {
-    
+
     boolean firstCycle;
     double startTime;
     double waitTime;
     CheesyVisionServer cheese;
-    
+
     public AutonomousHotWait() {
         cheese = CheesyVisionServer.getInstance();
-        
+
         this.reset();
     }
-    
+
     public void reset() {
         firstCycle = true;
-        cheese.reset();
     }
 
     public boolean run() {
-        if (cheese.getLeftStatus() || cheese.getRightStatus())
-        {
-            System.err.println("Wait 3");
-            waitTime = 4.0;
-        } else {
-            System.err.println("Wait 0.5");
-            waitTime = 0.5;
-        }
-        
-        if(firstCycle)
-        {
+
+        if (firstCycle) {
             System.err.print("Wait Start: ");
             startTime = Timer.getFPGATimestamp();
+            System.err.println("L: "+cheese.getLeftCount() + " R: "+cheese.getRightCount());
+            if (cheese.getLeftCount() + cheese.getRightCount() > 15) {
+                System.err.println("Wait 3");
+                waitTime = 4.0;
+            } else {
+                System.err.println("Wait 0.5");
+                waitTime = 0.5;
+            }
+
             firstCycle = false;
         }
         if (waitTime < (Timer.getFPGATimestamp() - startTime)) {
@@ -46,5 +45,5 @@ public class AutonomousHotWait extends AutonomousCommand {
         driverInput.updateAutonData(new Hashtable());
         return false;
     }
-    
+
 }
